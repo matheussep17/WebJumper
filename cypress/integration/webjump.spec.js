96bdc724c2d4786fb223c0af1888678bfd0615ac
Matheus <matheussep17@gmail.com>
2022-04-07 14:11:41 -0300

    tela principal

--- cypress/integration/webjump.spec.js
+++ cypress/integration/webjump.spec.js
@@ -1,32 +1,13 @@
-describe("Test", () => {
-  it("Cenário 1", () => {
-    cy.visit("https://wj-qa-automation-test.github.io/qa-test/");
-    cy.contains("One").click();
-    cy.contains("Two").click();
-    cy.contains("Four").click();
-    cy.contains("One").should("not.visible");
-    cy.contains("Two").should("not.visible");
-    cy.contains("Four").should("not.visible");
+describe("Teste técnico webjumper", () => {
+  it("Teste cenário 1", () => {
+    cy.cenario1();
   });
 
-  it("Cenário 2", () => {
-    cy.visit("https://wj-qa-automation-test.github.io/qa-test/");
-    cy.get("#panel_body_one").within(() => {
-      cy.contains("One").click();
-      cy.contains("Two").click();
-      cy.contains("Four").click();
-      cy.contains("One").should("not.visible");
-      cy.contains("Two").should("not.visible");
-      cy.contains("Four").should("not.visible");
-    });
+  it("Teste cenário 2", () => {
+    cy.cenario2();
   });
 
-  it("Cenário 3", () => {
-    cy.visit("https://wj-qa-automation-test.github.io/qa-test/");
-    cy.get("#first_name").type("Matheus");
-    cy.contains("One").click();
-    cy.contains("OptionThree").click();
-    cy.get('#select_box').select('option_two')
-    cy.xpath("//img[@alt='selenium']").should("be.visible");
+  it("Teste cenário 3", () => {
+    cy.cenario3();
   });
 });
