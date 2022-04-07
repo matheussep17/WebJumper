f13f9e52734b63a63aedb5247676eb949b7f12d3
Matheus <matheussep17@gmail.com>
2022-04-07 14:11:55 -0300

    imports

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,21 +1,9 @@
-// ***********************************************************
-// This example support/index.js is processed and
-// loaded automatically before your test files.
-//
-// This is a great place to put global configuration and
-// behavior that modifies Cypress.
-//
-// You can change the location of this file or turn off
-// automatically serving support files with the
-// 'supportFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/configuration
-// ***********************************************************
-
-// Import commands.js using ES2015 syntax:
-import './commands'
-require('cypress-xpath')
-
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
+import "./cenarios/cenário1";
+import "./cenarios/cenário2";
+import "./cenarios/cenário3";
+import "./commands";
+import "./comandos/assert";
+import "./comandos/clicar";
+import "./comandos/select";
+import "./comandos/write";
+require("cypress-xpath");
