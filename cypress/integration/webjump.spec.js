6f768a3e8e075ec2d60e387455b800a0de992bd9
Matheus <matheussep17@gmail.com>
2022-04-07 11:57:38 -0300

    Testes completos

---
+++ cypress/integration/webjump.spec.js
@@ -0,0 +1,32 @@
+describe("Test", () => {
+  it("Cenário 1", () => {
+    cy.visit("https://wj-qa-automation-test.github.io/qa-test/");
+    cy.contains("One").click();
+    cy.contains("Two").click();
+    cy.contains("Four").click();
+    cy.contains("One").should("not.visible");
+    cy.contains("Two").should("not.visible");
+    cy.contains("Four").should("not.visible");
+  });
+
+  it("Cenário 2", () => {
+    cy.visit("https://wj-qa-automation-test.github.io/qa-test/");
+    cy.get("#panel_body_one").within(() => {
+      cy.contains("One").click();
+      cy.contains("Two").click();
+      cy.contains("Four").click();
+      cy.contains("One").should("not.visible");
+      cy.contains("Two").should("not.visible");
+      cy.contains("Four").should("not.visible");
+    });
+  });
+
+  it("Cenário 3", () => {
+    cy.visit("https://wj-qa-automation-test.github.io/qa-test/");
+    cy.get("#first_name").type("Matheus");
+    cy.contains("One").click();
+    cy.contains("OptionThree").click();
+    cy.get('#select_box').select('option_two')
+    cy.xpath("//img[@alt='selenium']").should("exist");
+  });
+});
