299a6ae9db7f893d2f144b27bb605a571566258e
Matheus <matheussep17@gmail.com>
2022-04-07 16:24:43 -0300

    click iframe

--- cypress/support/comandos/clicar.js
+++ cypress/support/comandos/clicar.js
@@ -1,3 +1,7 @@
 Cypress.Commands.add("click_by_contains", (text) => {
   cy.contains(text).click();
 });
+
+Cypress.Commands.add("click_button_iframe", (id_frame, id) => {
+  cy.get(id_frame).iframe().get(id).click();
+});
